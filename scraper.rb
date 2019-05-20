377ca66e9b45520fb08b641c4aa3d118035769f6
Matthew Landauer <mlandauer@gmail.com>
2019-05-20 12:37:58 +1000

    Use library completely now

--- scraper.rb
+++ scraper.rb
@@ -4,8 +4,6 @@ scraper = EpathwayScraper::Scraper.new(
   "https://epathway-web.gleneira.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
 )
 
-page = scraper.pick_type_of_search(:all)
-
-scraper.scrape_all_index_pages_with_gets(4) do |record|
+scraper.scrape(list_type: :all, with_gets: true, max_pages: 4) do |record|
   EpathwayScraper.save(record)
 end
