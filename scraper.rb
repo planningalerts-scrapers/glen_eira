7e89964a0faa1ba29bceb7cfc6e5314d08cdbc01
Matthew Landauer <mlandauer@gmail.com>
2019-05-20 12:20:57 +1000

    Use more of the library

--- scraper.rb
+++ scraper.rb
@@ -6,13 +6,6 @@ scraper = EpathwayScraper::Scraper.new(
 
 page = scraper.pick_type_of_search(:all)
 
-# Only scrape the most recent 4 pages, not the entire database
-number_pages = 4
-(1..number_pages).each do |no|
-  url = "https://epathway-web.gleneira.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquirySummaryView.aspx?PageNumber=#{no}"
-  page = scraper.agent.get(url)
-  puts "Scraping page #{no} of " + number_pages.to_s + "..."
-  scraper.scrape_index_page(page) do |record|
-    EpathwayScraper.save(record)
-  end
+scraper.scrape_all_index_pages_with_gets(4) do |record|
+  EpathwayScraper.save(record)
 end
